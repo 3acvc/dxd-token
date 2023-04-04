98192c90dd1c8086ba5dc5b775a4273b4cdf992a
Adam Azad <adam@adamazad.com>
2023-04-04 19:03:49 +0100

    chore: hardcode test variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,16 +11,26 @@ import './tasks';
 
 // Load environment variables.
 dotenv.config();
-const { MNEMONIC, PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+const { MNEMONIC, PRIVATE_KEY, ALCHEMY_API_KEY, ETHER_SCAN_API_KEY } = process.env;
 
+const DEFAULT_PRIVATE_KEY = 'e41f686a80c3b013a7de09bcc1465674151ec9eec989b1d2d4884a819809372c';
 const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
 
+const privateKey = PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+const mnemonic = MNEMONIC || DEFAULT_MNEMONIC;
+
+
+let rpcURL = `https://eth.llamarpc.com`;
+if (ALCHEMY_API_KEY) {
+  rpcURL = `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`;
+}
+
 const sharedNetworkConfig: HttpNetworkUserConfig = {};
-if (PRIVATE_KEY) {
-  sharedNetworkConfig.accounts = [PRIVATE_KEY];
+if (privateKey.trim() !== '') {
+  sharedNetworkConfig.accounts = [privateKey];
 } else {
   sharedNetworkConfig.accounts = {
-    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+    mnemonic,
   };
 }
 
@@ -35,19 +45,18 @@ const config: HardhatUserConfig = {
       initialBaseFeePerGas: 0,
       chainId: 1,
       forking: {
-        url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}?blockNumber=16969691`,
-        // blockNumber: 16969691,
+        url: `${rpcURL}/?blockNumber=16969691`,
         enabled: true,
       },
       accounts: [
         {
-          privateKey: PRIVATE_KEY!,
+          privateKey,
           balance: utils.parseEther('10000').toString(),
         },
       ],
     },
     mainnet: {
-      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      url: rpcURL,
       chainId: 1,
     },
   },
@@ -62,7 +71,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      mainnet: process.env.ETHER_SCAN_API_KEY!,
+      mainnet: ETHER_SCAN_API_KEY!,
     },
   },
   typechain: {
